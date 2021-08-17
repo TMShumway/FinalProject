2641373431fd3c87c1b1e47f1402560af315a1b6
TMShumway <ryujindra@gmail.com>
2021-08-17 14:14:07 -0600

    Stub Spring Boot/REST project connect project dependencies

---
+++ MealTeam6/src/main/java/com/skilldistillery/mealteam6/MealTeam6Application.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mealteam6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealTeam6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealTeam6Application.class, args);
+	}
+
+}
