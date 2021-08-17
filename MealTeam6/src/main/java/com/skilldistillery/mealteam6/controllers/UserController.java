a328e444b107fbb49c25517466ccfd91cb17fe44
Brandon <files.brandon@gmail.com>
2021-08-17 16:40:38 -0500

    Configure Spring datasource

--- MealTeam6/src/main/java/com/skilldistillery/mealteam6/controllers/UserController.java
+++ MealTeam6/src/main/java/com/skilldistillery/mealteam6/controllers/UserController.java
@@ -1,9 +1,14 @@
 package com.skilldistillery.mealteam6.controllers;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.mealteam6.entities.User;
 import com.skilldistillery.mealteam6.services.UserService;
 
 @RestController
@@ -12,5 +17,14 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@GetMapping("users/{username}")
+	public User getUserByUsername(
+			@PathVariable String username,
+			HttpServletResponse res
+			) {
+		User user = userService.userByUsername(username);
+		return user;
+	}
 
 }
--- MealTeam6/src/main/java/com/skilldistillery/mealteam6/services/UserService.java
+++ MealTeam6/src/main/java/com/skilldistillery/mealteam6/services/UserService.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.mealteam6.services;
 
+import com.skilldistillery.mealteam6.entities.User;
+
 public interface UserService {
 
+	User userByUsername(String username);
+	
 }
