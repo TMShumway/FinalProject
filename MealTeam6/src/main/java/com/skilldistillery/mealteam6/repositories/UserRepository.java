86ddf32796564740801823dc18dbdf30a09d1059
TMShumway <ryujindra@gmail.com>
2021-08-17 14:27:03 -0600

    Configure Spring REST Project

---
+++ MealTeam6/src/main/java/com/skilldistillery/mealteam6/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.mealteam6.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.mealteam6.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
---
+++ MealTeam6/src/main/java/com/skilldistillery/mealteam6/services/UserService.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.mealteam6.services;
+
+public interface UserService {
+
+}
