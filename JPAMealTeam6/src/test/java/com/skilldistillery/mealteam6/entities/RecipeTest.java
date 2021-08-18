b6526768f5077c27191ceb53ec7e174cd3ce6134
Brandon <files.brandon@gmail.com>
2021-08-18 17:16:57 -0500

    recipe tests for entity relationships

--- JPAMealTeam6/src/test/java/com/skilldistillery/mealteam6/entities/RecipeTest.java
+++ JPAMealTeam6/src/test/java/com/skilldistillery/mealteam6/entities/RecipeTest.java
@@ -53,9 +53,41 @@ class RecipeTest {
 		assertEquals("https://unsplash.com/photos/Ucwd8w-JHwM",  recipe.getRecipeImages().get(0).getImageUrl());
 		
 	}
-//	@Test
-//	void test_recipe_rating_entity_mapping() {}
-//	
+	@Test
+	void test_recipe_rating_entity_mapping() {
+		assertNotNull(recipe.getRatings());
+		assertEquals(5, recipe.getRatings().get(0).getStarRating());
+		
+	}
+	@Test
+	void test_recipe_comment_entity_mapping() {
+		assertNotNull(recipe.getRecipeComments());
+		assertEquals("THIS IS COOL!!!", recipe.getRecipeComments().get(0).getDetails());
+		
+	}
+	@Test
+	void test_recipe_categories_entity_mapping() {
+		assertNotNull(recipe.getCategories());
+		assertEquals("Seafood", recipe.getCategories().get(0).getCategoryName());
+		
+	}
+	@Test
+	void test_recipe_ingredients_entity_mapping() {
+		assertNotNull(recipe.getIngredients());
+		assertEquals("eggs", recipe.getIngredients().get(0).getName());
+		
+	}
+	@Test
+	void test_recipe_restrictions_entity_mapping() {
+		assertNotNull(recipe.getRestrictions());
+		assertEquals("Gluten-Free", recipe.getRestrictions().get(0).getName());
+		
+	}
+	
+	
+	
+	
+	
 	
 	
 	
