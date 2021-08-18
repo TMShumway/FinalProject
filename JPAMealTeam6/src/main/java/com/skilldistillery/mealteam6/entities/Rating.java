13c34d9800b27f79657120194a5fd84c1fb8ca8e
TMShumway <ryujindra@gmail.com>
2021-08-18 09:50:34 -0600

    Create composite key class and associated entity JUnit test

--- JPAMealTeam6/src/main/java/com/skilldistillery/mealteam6/entities/Rating.java
+++ JPAMealTeam6/src/main/java/com/skilldistillery/mealteam6/entities/Rating.java
@@ -1,12 +1,63 @@
 package com.skilldistillery.mealteam6.entities;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-
+@Entity
 public class Rating {
 
+	@EmbeddedId
+	private RatingId id;
+	
+	@Column(name = "star_rating")
+	private int starRating;
+
+	public RatingId getId() {
+		return id;
+	}
+
+	public void setId(RatingId id) {
+		this.id = id;
+	}
+
+	public int getStarRating() {
+		return starRating;
+	}
+
+	public void setStarRating(int starRating) {
+		this.starRating = starRating;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rating other = (Rating) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Rating [id=" + id + ", starRating=" + starRating + "]";
+	}
+	
 	
 }
---
+++ JPAMealTeam6/src/test/java/com/skilldistillery/mealteam6/entities/RatingTest.java
@@ -0,0 +1,54 @@
+package com.skilldistillery.mealteam6.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RatingTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Rating rating;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMealTeam6");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		RatingId rid = new RatingId();
+		rid.setRecipeID(1);
+		rid.setUserID(1);
+		rating = em.find(Rating.class, rid);
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		rating = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(rating);
+		assertEquals(5, rating.getStarRating());
+	}
+
+}
